bf09badb582025642d51fca42e7b3d07af5824a5
EC2 Default User <ec2-user@ip-10-225-24-179.ec2.internal>
2016-02-09 12:00:22 +0000

    Instrumented by Insights Service for uploading the artifacts

--- df-build.sh
+++ df-build.sh
@@ -7,4 +7,4 @@ echo "Zipping artifacts begin..."
 zip libs.zip $(git ls-files -o | grep -e target/dependency/.*jar) || { echo "Error: Zipping libs failed"; exit 1; }
 zip build.zip $(git ls-files -o | grep -e target/classes/.*class) || { echo "Error: Zipping binaries failed"; exit 1; }
 echo "Zipping artifacts DONE!!!"
-# Modified by Insights Service at 2016-02-08 15:51:10.474166
+# Modified by Insights Service at 2016-02-09 12:00:21.902073
